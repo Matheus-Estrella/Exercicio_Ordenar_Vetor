9a25f06dd4691e73837bb96514defcae160b9f1e
Matheus-Estrella <matheus.a.estrella@gmail.com>
2024-04-23 23:35:50 -0300

    Deu certo

--- Exercicio_Ordenar_Vetor/Program.cs
+++ Exercicio_Ordenar_Vetor/Program.cs
@@ -1,11 +1,12 @@
 ﻿/*
  3-) Faça um programa que leia cinco números, guarde-os num vetor, e ordene-os de forma crescente em um novo vetor.
  */
-const int MAX = 5;
+const int MAX = 50;
 float[] numeros = new float[MAX];
 int i, j=(MAX-1), maior = 0, op = 0;
 float aux = 0;
 float[] numeros_ordenados = new float[MAX];
+float[] numeros_sem_repetir = new float[MAX];
 
 do
 {
@@ -26,6 +27,9 @@ for (i = 0; i < MAX; i++)
     }
     numeros_ordenados[i] = numeros[i];
 }
+
+// Ordenando ...
+
 do
 {
     for (i = 0; i < j; i++)
@@ -39,9 +43,44 @@ do
     }
     j--;
 }while(j>=0);
+
 Console.Write("\nOs números em ordem crescente são: ");
+
 for (i = 0; i < MAX; i++)
 {
     Console.Write($"{numeros_ordenados[i]} ");
+}
+Console.WriteLine();
+
+
+////REMOVENDO REPETIÇÕES...
+
+j = 0;
+
+for(i=1;i<MAX; i++)
+{
+    if ((numeros_ordenados[i] != numeros_ordenados[i-1]))
+    {
+        numeros_sem_repetir[j] = numeros_ordenados[i - 1];
+        j++;
+    }
+    if(i == MAX - 1)
+    {
+        numeros_sem_repetir[j] = numeros_ordenados[i];
+    }
+}
+Console.Write("\nOs números em ordem crescente não repetidos são: ");
+
+for (i = 0; i <= j; i++)
+{
+    Console.Write($"{numeros_sem_repetir[i]} ");
+}
+Console.WriteLine();
+
+Console.Write("\nOs números em ordem decrescente não repetidos são: ");
+
+for (i = 0; i <= j; i++)
+{
+    Console.Write($"{numeros_sem_repetir[j-i]} ");
 }
 Console.WriteLine();
--- Exercicio_Ordenar_Vetor/obj/Debug/net6.0/Exercicio_Ordenar_Vetor.AssemblyInfo.cs
+++ Exercicio_Ordenar_Vetor/obj/Debug/net6.0/Exercicio_Ordenar_Vetor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio_Ordenar_Vetor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f511e68b6a3e47db5f31af46b42b38f840f9fa17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c6c4517b9f9311bbea7f26c60ef51c35fd580b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio_Ordenar_Vetor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio_Ordenar_Vetor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
